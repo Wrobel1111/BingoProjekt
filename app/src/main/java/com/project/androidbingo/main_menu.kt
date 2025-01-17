1a45b690bbb79414360d33d7c7cd2a7d5c00c842
Wrobel1111 <konarzewski32@gmail.com>
2025-01-17 16:11:31 +0100

    Przygotowanie głównego ekranu
    Prymitywny layout zgodnie z designem,
    zmiany nazw na main_menu i podobne. Próbuję
    robić kolejny z aktywnym bingo.

--- app/src/main/java/com/project/androidbingo/main_menu.kt
+++ app/src/main/java/com/project/androidbingo/main_menu.kt
@@ -5,15 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.project.androidbingo.databinding.FragmentFirstBinding
+import com.project.androidbingo.databinding.MainMenuBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MainMenu : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: MainMenuBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,17 +23,16 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = MainMenuBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        //binding.buttonFirst.setOnClickListener {
+        //    findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        //}
     }
 
     override fun onDestroyView() {
