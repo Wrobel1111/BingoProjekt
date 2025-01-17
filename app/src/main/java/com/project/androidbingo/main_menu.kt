b6bd085b7d3f2036c8fbe8e42224eff5e6754ff8
Wrobel1111 <konarzewski32@gmail.com>
2025-01-17 18:43:03 +0100

    MutableList przycisków
    Przygotowanie listy przycisków w kodzie do
    generowania widoków danych bingo. Łatwo z
    pętelki będzie można na żywca dodawać im
    konkretne cechy. Liczba przycisków obecnie
    na sztywno.

--- app/src/main/java/com/project/androidbingo/main_menu.kt
+++ app/src/main/java/com/project/androidbingo/main_menu.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
 import com.project.androidbingo.databinding.MainMenuBinding
+import org.w3c.dom.Text
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -18,12 +21,15 @@ class MainMenu : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val numOfButtons = 4; //liczba przycisków do bingo na ekranie głównym
+                                  //Chwilowo będzie na sztywno
+    val allButtons = mutableListOf<Button>();
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
+        //val packageName =
         _binding = MainMenuBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -34,6 +40,14 @@ class MainMenu : Fragment() {
         //binding.buttonFirst.setOnClickListener {
         //    findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         //}
+
+        //val abc = view.findViewById<Button>(R.id.button_bingo_1).id;
+        //val  def : TextView = view.findViewById<TextView>(R.id.textView);
+        //def.text = view.resources.getIdentifier("button_bingo_"+1,"id", "com.project.androidbingo").toString();
+        for (i in 1..numOfButtons)
+        {
+            allButtons.add(view.findViewById(view.resources.getIdentifier("button_bingo_"+i,"id", "com.project.androidbingo")))
+        }
     }
 
     override fun onDestroyView() {
