9b131d228307be1e6a67548f19aac141a64e1ddd
Wrobel1111 <konarzewski32@gmail.com>
2025-01-31 15:09:53 +0100

    Revert "usuniecie niepotrzebnych plikow / oznaczenie obecnych"
    This reverts commit cfd2c37d04cffe412a20fdbee9679c4b5c9ee1ea.

--- app/src/main/java/com/project/androidbingo/ui/login/LoggedInUserView.kt
+++ app/src/main/java/com/project/androidbingo/ui/login/LoggedInUserView.kt
@@ -3,8 +3,6 @@ package com.project.androidbingo.ui.login
 /**
  * User details post authentication that is exposed to the UI
  */
-// zostawiamy do przyszlych celow rozbudowy aplikacji
-
 data class LoggedInUserView(
     val displayName: String
     //... other data fields that may be accessible to the UI
--- app/src/main/java/com/project/androidbingo/ui/login/LoginFormState.kt
+++ app/src/main/java/com/project/androidbingo/ui/login/LoginFormState.kt
@@ -3,8 +3,6 @@ package com.project.androidbingo.ui.login
 /**
  * Data validation state of the login form.
  */
-// zostawiamy do przyszlych celow rozbudowy aplikacji
-
 data class LoginFormState(
     val usernameError: Int? = null,
     val passwordError: Int? = null,
